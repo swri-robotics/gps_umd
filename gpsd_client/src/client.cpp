d5f7aee6e5743744aaa66e1f1d88c32b255ab8fd
David Anthony <david.anthony@swri.org>
2024-05-09 13:47:39 -0500

    Port of PR #89

--- gpsd_client/src/client.cpp
+++ gpsd_client/src/client.cpp
@@ -59,7 +59,12 @@ class GPSDClient {
       if (!gps->waiting(1e6))
         return;
 
-      gps_data_t *p = gps->read();
+      // Read out all queued data and only act on the latest
+      gps_data_t* p = NULL;
+      while (gps_->waiting(0))
+      {
+        p = gps_->read();
+      }
 #else
       gps_data_t *p = gps->poll();
 #endif

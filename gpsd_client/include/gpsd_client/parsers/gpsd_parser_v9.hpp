79e911e54e5a4d40a91e4b68d996ab89cdbf186f
David Anthony <djanthony@gmail.com>
2026-07-14 16:33:29 -0500

    Update gpsd_client/include/gpsd_client/parsers/gpsd_parser_v9.hpp
    Co-authored-by: DangitBen <30333928+DangitBen@users.noreply.github.com>

--- gpsd_client/include/gpsd_client/parsers/gpsd_parser_v9.hpp
+++ gpsd_client/include/gpsd_client/parsers/gpsd_parser_v9.hpp
@@ -19,7 +19,7 @@ public:
   using GpsdParserBase::GpsdParserBase;
 
 protected:
-  int getFixStatus(const gps_data_t& data) const override
+  [[nodiscard]] int getFixStatus(const gps_data_t& data) const override
   {
     return data.status;
   }
